45ee7542f53a5e989812b315a6c6d96b6dd52eb3
Tdual <colordir@gmail.com>
2016-08-10 15:24:25 +0900

    rename argument in dfutil's function

--- util/dfutil.py
+++ util/dfutil.py
@@ -1,25 +1,23 @@
 import pandas as pd
 
-def get_sma(df, date_interval):
+def get_sma(df, period):
     ser = pd.Series(df)
-    sma = ser.rolling(window=date_interval,center=False).mean()
+    sma = ser.rolling(window=period,center=False).mean()
     return sma
 
-def get_std(df, date_interval):
-    di= date_interval
+def get_std(df, period):
     ser = pd.Series(df)
-    std = ser.rolling(window=di,min_periods=di,center=False).std()
+    std = ser.rolling(window=period,min_periods=period,center=False).std()
     return std
 
 
-def get_bb(df, date_interval=20, coeff=2):
+def get_bb(df, period=50, coeff=2):
     u"""
     getting Bollinger Bands
     """
 
-    di= date_interval
-    sma = get_sma(df, di)
-    std = get_std(df, di)
+    sma = get_sma(df, period)
+    std = get_std(df, period)
     upper = sma + coeff * std
     lower = sma - coeff * std
     bb = pd.concat([df, upper, sma, lower], axis=1)
